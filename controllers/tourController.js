b1e8b82bf6c5a1abb60194c056789e0a3738e475
Senadin <beganovicsenadin24@gmail.com>
2020-08-27 21:10:40 +0200

    added more middlewares to tourController

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -5,7 +5,6 @@ const tours = JSON.parse(
 );
 
 exports.checkID = (req, res, next, val) => {
-  console.log(`Tour id is: ${val}`)
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
       status: "fail",
@@ -13,6 +12,16 @@ exports.checkID = (req, res, next, val) => {
     });
   }
 
+  next();
+}
+
+exports.checkBody = (req, res, next) => {
+  if(!req.body.name || !req.body.price){
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Missing name or price'
+    })
+  }
   next();
 }
 
@@ -61,7 +70,6 @@ exports.createTour = (req, res) => {
 };
 
 exports.updateTour = (req, res) => {
-
   res.status(200).json({
     status: "success",
     data: {
