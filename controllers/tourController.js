9c755426fe45e85abf65b19272bab921eb3c1363
Senadin <beganovicsenadin24@gmail.com>
2020-08-28 21:41:14 +0200

    implemented more advanced api filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,21 +2,31 @@ const Tour = require("./../models/tourModels");
 
 exports.getAllTours = async (req, res) => {
   try {
+    //BUILD QUERY
+    // 1) Filtering
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach(el => delete queryObj[el])
 
-    const query = Tour.find(queryObj);
+    // 2) Advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/(gte|gt|lte|lt)\b/g, match => `$${match}`)
+  
+    // {difficulty: 'easy', duration: { $gte: 5 }}
+    // gte, gt, lte, lt
+    
+    const query = Tour.find(JSON.parse(queryStr));
 
+    //EXECUTE QUERY
+    const tours = await query
+    
     // const query = await Tour.find()
     //   .where("duration")
     //   .equals(5)
     //   .where("difficulty")
     //   .equals("easy");
     
-    //EXECUTE QUERY
-    const tours = await query
-
+    // SEND RESPONSE
     res.status(200).json({
       status: "success",
       results: tours.length,
