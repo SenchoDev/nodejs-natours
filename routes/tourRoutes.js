f55ca3e78eec2c0cf5ab965443fc2dcdcb412d7b
Senadin <beganovicsenadin24@gmail.com>
2020-08-28 18:42:41 +0200

    creating data and sending the data to mongoDB database with mongoose

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -6,18 +6,14 @@ const {
   createTour,
   updateTour,
   deleteTour,
-  checkID,
-  checkBody,
 } = require("./../controllers/tourController");
 
 const router = express.Router();
 
 // router.param("id", checkID);
 
-router.route("/").get(getAllTours).post(checkBody, createTour);
+router.route("/").get(getAllTours).post(createTour);
 
 router.route("/:id").get(getTour).patch(updateTour).delete(deleteTour);
 
 module.exports = router;
-
-const x = 3;
