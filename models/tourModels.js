663e5ca643097a2136aaa3dccd8d5cf93418e83e
Senadin <beganovicsenadin24@gmail.com>
2020-09-03 19:59:25 +0200

    modelling tour guides embedding

--- models/tourModels.js
+++ models/tourModels.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const slugify = require("slugify");
+const User = require('./userModel')
 
 const tourSchema = new mongoose.Schema(
   {
@@ -99,9 +100,9 @@ const tourSchema = new mongoose.Schema(
         address: String,
         description: String,
         day: Number
-
       }
-    ]
+    ],
+    guides: Array
   },
   {
     toJSON: { virtuals: true },
@@ -118,6 +119,14 @@ tourSchema.pre("save", function (next) {
   this.slug = slugify(this.name, { lower: true });
   next();
 });
+
+tourSchema.pre("save", async function(next){
+  const guidesPromises = this.guides.map(async id => await User.findById(id));
+
+  this.guides = await Promise.all(guidesPromises)
+
+  next();
+})
 
 // tourSchema.pre('save', function(next){
 //   console.log('Will save document..');
