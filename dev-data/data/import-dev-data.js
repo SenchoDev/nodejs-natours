7ffacd2962f61d20d4d887f10eb08fb7e9cdb696
Senadin <beganovicsenadin24@gmail.com>
2020-09-03 16:16:51 +0200

    modeling locations (geospatial data). implementing embedded(denormalized) data

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -17,7 +17,7 @@ mongoose.connect(DB, {
   console.log('DB connnections successful!');
 });
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'));
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
 
 // IMPORT DATA INTO DB
 
