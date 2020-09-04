85f0c0061203cfa454d43b56a5b391d6b57e51dc
Senadin <beganovicsenadin24@gmail.com>
2020-09-04 12:16:36 +0200

    factory function update and create

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -4,7 +4,7 @@ const factory = require("./hanlderFactory");
 
 exports.getAllReviews = catchAsync(async (req, res, next) => {
   let filter = {};
-  if(req.params.tourId) filter = { tour: req.params.tourId }
+  if (req.params.tourId) filter = { tour: req.params.tourId };
 
   const reviews = await Review.find(filter);
 
@@ -17,18 +17,15 @@ exports.getAllReviews = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.createReview = catchAsync(async (req, res, next) => {
+exports.setTourUserIds = (req, res, next) => {
   // Allow nested routes
   if (!req.body.tour) req.body.tour = req.params.tourId;
   if (!req.body.user) req.body.user = req.user.id;
-  const newReview = await Review.create(req.body);
+  next();
+};
 
-  res.status(200).json({
-    status: 'success',
-    data: {
-      review: newReview,
-    }
-  })
-})
+exports.createReview = factory.createOne(Review);
+
+exports.updateReview = factory.updateOne(Review);
 
 exports.deleteReview = factory.deleteOne(Review);
