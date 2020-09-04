8b5e3afce41f420feb9ec68c2e44c11c9f0f08f6
Senadin <beganovicsenadin24@gmail.com>
2020-09-04 20:02:45 +0200

    calculating average rating on update

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -45,42 +45,62 @@ const reviewSchema = new mongoose.Schema(
 //   next();
 // })
 
-reviewSchema.pre(/^find/, function (next){
+reviewSchema.pre(/^find/, function (next) {
   this.populate({
-    path: 'user',
-    select: 'name photo'
+    path: "user",
+    select: "name photo",
   });
 
   next();
-})
+});
 
-reviewSchema.statics.calcAverageRatings = async function(tourId){
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
   const stats = await this.aggregate([
     {
-      $match: {tour: tourId}
+      $match: { tour: tourId },
     },
     {
       $group: {
-        _id: '$tour',
+        _id: "$tour",
         nRating: { $sum: 1 },
-        avgRating: {$avg: '$rating'}
-      }
-    }
+        avgRating: { $avg: "$rating" },
+      },
+    },
   ]);
-  
-  await Tour.findByIdAndUpdate(tourId, {
-    ratingsQuantity: stats[0].nRating,
-    ratingsAverage: stats[0].avgRating
-  })
 
-  console.log(stats)
-}
+  //console.log(stats);
 
-reviewSchema.post('save', function(){
+  if (stats.length > 0) {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: stats[0].nRating,
+      ratingsAverage: stats[0].avgRating,
+    });
+  } else {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: 0,
+      ratingsAverage: 4.5,
+    });
+  }
+};
+
+reviewSchema.post("save", function () {
   // this points to current review
   this.constructor.calcAverageRatings(this.tour);
-})
+});
 
+// findByIdAndUpdate
+// findByIdAndDelete
+
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+  this.r = await this.findOne();
+  //console.log(this.r);
+  next();
+});
+
+reviewSchema.post(/^findOneAnd/, async function () {
+  // await this.findOne(); does not work here, query has already been executed
+  await this.r.constructor.calcAverageRatings(this.r.tour);
+});
 
 const Review = mongoose.model("Review", reviewSchema);
 
@@ -89,4 +109,3 @@ module.exports = Review;
 // NESTED ROUTES
 //POST /tour/242423/reviews
 //GET /tour/242423/reviews/234242342
-
