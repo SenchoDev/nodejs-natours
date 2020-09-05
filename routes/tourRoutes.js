28e6d8434194b2ab45671e84a28691d1b1cad616
Senadin <beganovicsenadin24@gmail.com>
2020-09-05 11:58:10 +0200

    added geospatial functionality, finsihed with the api, started working on views of website with pug templates.

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -9,9 +9,12 @@ const {
   aliasTopTours,
   getTourStats,
   getMonthlyPlan,
+  getToursWithin,
+  getDistances
 } = require("./../controllers/tourController");
 const { protect, restrictTo } = require("./../controllers/authController");
-const reviewRouter = require("./../routes/reviewRoutes")
+const reviewRouter = require("./../routes/reviewRoutes");
+const { route } = require("./../routes/reviewRoutes");
 
 const router = express.Router();
 
@@ -19,6 +22,10 @@ const router = express.Router();
 
 router.route("/tour-stats").get(getTourStats);
 router.route("/monthly-plan/:year").get(protect, restrictTo("admin", "lead-guide", 'guide'),getMonthlyPlan);
+
+router.route('/tours-within/:distance/center/:latlng/unit/:unit').get(getToursWithin)
+
+router.route('/distances/:latlng/unit/:unit').get(getDistances);
 
 router.route("/top-5-cheap").get(aliasTopTours, getAllTours);
 
