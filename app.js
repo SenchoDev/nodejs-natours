7789ef43b2f00d669773dd3a2f6e4d7c161ead14
Senadin <beganovicsenadin24@gmail.com>
2020-09-06 10:32:06 +0200

    using pug templating to for views

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const path = require("path")
+const path = require("path");
 const express = require("express");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
@@ -11,12 +11,16 @@ const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
-const reviewRouter = require("./routes/reviewRoutes")
+const reviewRouter = require("./routes/reviewRoutes");
+const viewRouter = require("./routes/viewRoutes");
+
 
 const app = express();
 
-app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, 'views'))
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+// Serving static files
+app.use(express.static(path.join(__dirname, "public")));
 
 // 1) GLOBAL MIDDLEWARES
 // Set security HTTP header
@@ -57,13 +61,10 @@ app.use(
   })
 );
 
-// Serving static files
-app.use(express.static(`${__dirname}/public`));
-
-app.use("/api/v1/tours", tourRouter);
+app.use("/", viewRouter);
+app.use("/api/v1/tours", tourRouter);1
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/reviews", reviewRouter);
-
 
 app.all("*", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
