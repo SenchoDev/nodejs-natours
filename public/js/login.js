410a61e7dbf4d3403bf12d9e2013a73d2081ef14
Senadin <beganovicsenadin24@gmail.com>
2020-09-08 10:05:03 +0200

    finsihed authentication on front end, without react(gonna implement react last)

--- public/js/login.js
+++ public/js/login.js
@@ -1,8 +1,9 @@
 
-//import { showAlert } from './alerts';
+import axios from 'axios';
+import { showAlert } from './alerts'
 
 
-const login = async (email, password) => {
+export const login = async (email, password) => {
   
   try {
      const res = await axios({
@@ -13,18 +14,17 @@ const login = async (email, password) => {
         password
       }
     })
+    if(res.data.status === 'success'){
+      showAlert('success', 'Logged in successfully!');
+      window.setTimeout(() => {
+        location.assign('/')
+      }, 1500)
+    }
     console.log(res)
   } catch (err) {
-    console.log(err.response.data)
+    showAlert('error',err.response.data.message)
   }
 };
-
-document.querySelector('.form').addEventListener('submit', e => {
-  e.preventDefault();
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-  login(email, password)
-})
 
 // export const logout = async () => {
 //   try {
--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,45 +1,45 @@
-const locations = JSON.parse(document.getElementById("map").dataset.locations);
+export const displayMap = (locations) => {
+  mapboxgl.accessToken =
+    "pk.eyJ1Ijoic2VuY2hvZGV2diIsImEiOiJja2VxeHpoZmQwd3hnMndvZWkycndyMmtwIn0.6eHRycUIWzFIhhBXRduYgg";
+  var map = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/senchodevv/ckesh450n72v619k9hi3rl8hx",
+    scrollZoom: false,
+  });
 
-mapboxgl.accessToken =
-  "pk.eyJ1Ijoic2VuY2hvZGV2diIsImEiOiJja2VxeHpoZmQwd3hnMndvZWkycndyMmtwIn0.6eHRycUIWzFIhhBXRduYgg";
-var map = new mapboxgl.Map({
-  container: "map",
-  style: "mapbox://styles/senchodevv/ckesh450n72v619k9hi3rl8hx",
-  scrollZoom: false,
-});
+  const bounds = new mapboxgl.LngLatBounds();
 
-const bounds = new mapboxgl.LngLatBounds();
+  locations.forEach((loc) => {
+    // Create marker
+    const el = document.createElement("div");
+    el.className = "marker";
 
-locations.forEach((loc) => {
-  // Create marker
-  const el = document.createElement("div");
-  el.className = "marker";
+    // Add marker
+    new mapboxgl.Marker({
+      element: el,
+      anchor: "bottom",
+    })
+      .setLngLat(loc.coordinates)
+      .addTo(map);
 
-  // Add marker
-  new mapboxgl.Marker({
-    element: el,
-    anchor: "bottom",
-  })
-    .setLngLat(loc.coordinates)
-    .addTo(map);
+    //Add popup
+    new mapboxgl.Popup({
+      offset: 30,
+    })
+      .setLngLat(loc.coordinates)
+      .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+      .addTo(map);
 
-  //Add popup
-  new mapboxgl.Popup({
-    offset: 30,
-  })
-    .setLngLat(loc.coordinates)
-    .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
-    .addTo(map);
+    // Extend map bounds to include current location
+    bounds.extend(loc.coordinates);
+  });
 
-  // Extend map bounds to include current location
-  bounds.extend(loc.coordinates);
-});
-
-map.fitBounds(bounds, {
-  padding: {
-    top: 200,
-    bottom: 150,
-    left: 100,
-    right: 100,
-  },
-});
+  map.fitBounds(bounds, {
+    padding: {
+      top: 200,
+      bottom: 150,
+      left: 100,
+      right: 100,
+    },
+  });
+};
