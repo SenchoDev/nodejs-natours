b6a646b5d6379d88be39ef61988f581b27eadf5d
Senadin <beganovicsenadin24@gmail.com>
2020-09-08 08:52:20 +0200

    added more login functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,17 +15,16 @@ const signToken = (id) => {
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
 
-  const cookieOptions =  {
+  const cookieOptions = {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-  }
+  };
 
-  if(process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+  if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
 
   res.cookie("jwt", token, cookieOptions);
-
 
   // Remove the password from the output
   user.password = undefined;
@@ -72,6 +71,8 @@ exports.protect = catchAsync(async (req, res, next) => {
     req.headers.authorization.startsWith("Bearer")
   ) {
     token = req.headers.authorization.split(" ")[1];
+  } else if (req.cookies.jwt) {
+    token = req.cookies.jwt;
   }
   if (!token) {
     return next(
@@ -104,6 +105,37 @@ exports.protect = catchAsync(async (req, res, next) => {
   req.user = currentUser;
   next();
 });
+
+// Only for rendered pages, no error
+exports.isLoggedIn = async (req, res, next) => {
+  if (req.cookies.jwt) {
+    try {
+      // 1) verify token
+      const decoded = await promisify(jwt.verify)(
+        req.cookies.jwt,
+        process.env.JWT_SECRET
+      );
+
+      // 2) Check if user still exists
+      const currentUser = await User.findById(decoded.id);
+      if (!currentUser) {
+        return next();
+      }
+
+      // 3) Check if user changed password after the token was issued
+      if (currentUser.changedPasswordAfter(decoded.iat)) {
+        return next();
+      }
+
+      // THERE IS A LOGGED IN USER
+      res.locals.user = currentUser;
+      return next();
+    } catch (err) {
+      return next();
+    }
+  }
+  next();
+};
 
 exports.restrictTo = (...roles) => {
   return (req, res, next) => {
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,10 +1,18 @@
 const express = require("express");
-const { getOverview, getTour, getLoginFrom} = require("../controllers/viewsController");
+const {
+  getOverview,
+  getTour,
+  getLoginFrom,
+  
+} = require("../controllers/viewsController");
 
+const { isLoggedIn } = require("./../controllers/authController");
 const router = express.Router();
+
+router.use(isLoggedIn);
 
 router.get("/", getOverview);
 router.get("/tour/:slug", getTour);
-router.get('/login', getLoginFrom)
+router.get("/login", getLoginFrom);
 
 module.exports = router;
