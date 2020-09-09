7e8bcae34d7a04d0bdf5f13413d0628a50673532
Senadin <beganovicsenadin24@gmail.com>
2020-09-09 15:30:41 +0200

    processing multiple images

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -10,7 +10,9 @@ const {
   getTourStats,
   getMonthlyPlan,
   getToursWithin,
-  getDistances
+  getDistances,
+  uploadTourImages,
+  resizeTourImages,
 } = require("./../controllers/tourController");
 const { protect, restrictTo } = require("./../controllers/authController");
 const reviewRouter = require("./../routes/reviewRoutes");
@@ -21,24 +23,37 @@ const router = express.Router();
 // router.param("id", checkID);
 
 router.route("/tour-stats").get(getTourStats);
-router.route("/monthly-plan/:year").get(protect, restrictTo("admin", "lead-guide", 'guide'),getMonthlyPlan);
+router
+  .route("/monthly-plan/:year")
+  .get(protect, restrictTo("admin", "lead-guide", "guide"), getMonthlyPlan);
 
-router.route('/tours-within/:distance/center/:latlng/unit/:unit').get(getToursWithin)
+router
+  .route("/tours-within/:distance/center/:latlng/unit/:unit")
+  .get(getToursWithin);
 
-router.route('/distances/:latlng/unit/:unit').get(getDistances);
+router.route("/distances/:latlng/unit/:unit").get(getDistances);
 
 router.route("/top-5-cheap").get(aliasTopTours, getAllTours);
 
-router.route("/").get(getAllTours).post(protect, restrictTo('admin', 'lead-guide'), createTour);
+router
+  .route("/")
+  .get(getAllTours)
+  .post(protect, restrictTo("admin", "lead-guide"), createTour);
 
 router
   .route("/:id")
   .get(getTour)
-  .patch(protect, restrictTo("admin", "lead-guide"), updateTour)
+  .patch(
+    protect,
+    restrictTo("admin", "lead-guide"),
+    uploadTourImages,
+    resizeTourImages,
+    updateTour
+  )
   .delete(protect, restrictTo("admin", "lead-guide"), deleteTour);
 
 // router.route("/:tourId/reviews").post(protect, restrictTo('user'), createReview);
 
-router.use('/:tourId/reviews', reviewRouter)
+router.use("/:tourId/reviews", reviewRouter);
 
 module.exports = router;
