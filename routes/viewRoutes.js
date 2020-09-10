c61f12652c537939161a223573a50420b9a358fc
Senadin <beganovicsenadin24@gmail.com>
2020-09-10 15:21:23 +0200

    rendering user booked tours

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -4,6 +4,7 @@ const {
   getTour,
   getLoginForm,
   getAccount,
+  getMyTours,
   updateUserData
 } = require("../controllers/viewsController");
 
@@ -17,6 +18,7 @@ router.get("/", createBookingCheckout, isLoggedIn, getOverview);
 router.get("/tour/:slug", isLoggedIn, getTour);
 router.get("/login", isLoggedIn, getLoginForm);
 router.get('/me', protect, getAccount);
+router.get('/my-tours', protect, getMyTours)
 
 router.post('/submit-user-data', protect, updateUserData)
 
