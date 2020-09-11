196faa7cb4c73302e5fb1cf3d867c2efb8aed91e
Senadin <beganovicsenadin24@gmail.com>
2020-09-11 15:12:17 +0200

    fixed stripe bugs

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,25 +1,23 @@
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
-const Tour = require("../models/tourModels");
-const User = require("../models/userModel");
-
-const Booking = require("../models/bookingModel");
-
-const catchAsync = require("../utils/catchAsync");
-const AppError = require("../utils/appError");
-const factory = require("./hanlderFactory");
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const Tour = require('../models/tourModel');
+const User = require('../models/userModel');
+const Booking = require('../models/bookingModel');
+const catchAsync = require('../utils/catchAsync');
+const factory = require('./handlerFactory');
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // 1) Get the currently booked tour
   const tour = await Tour.findById(req.params.tourId);
+  // console.log(tour);
 
-  // 2) Create checkout getCheckoutSession
+  // 2) Create checkout session
   const session = await stripe.checkout.sessions.create({
-    payment_method_types: ["card"],
-    // success_url: `${req.protocol}://${req.get("host")}/?tour=${
+    payment_method_types: ['card'],
+    // success_url: `${req.protocol}://${req.get('host')}/my-tours/?tour=${
     //   req.params.tourId
     // }&user=${req.user.id}&price=${tour.price}`,
-    success_url: `${req.protocol}://${req.get("host")}/my-tours`,
-    cancel_url: `${req.protocol}://${req.get("host")}/tour/${tour.slug}`,
+    success_url: `${req.protocol}://${req.get('host')}/my-tours?alert=booking`,
+    cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
     line_items: [
@@ -27,43 +25,33 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
         name: `${tour.name} Tour`,
         description: tour.summary,
         images: [
-          `${req.protocol}://${req.get("host")}/img/tours/${tour.imageCover}`,
+          `${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`
         ],
         amount: tour.price * 100,
-        currency: "usd",
-        quantity: 1,
-      },
-    ],
+        currency: 'usd',
+        quantity: 1
+      }
+    ]
   });
 
   // 3) Create session as response
   res.status(200).json({
-    status: "success",
-    session,
+    status: 'success',
+    session
   });
 });
 
-// exports.createBookingCheckout = catchAsync(async (req, res, next) => {
-//   // this is temporary, because its unsecure: everyone can make bookings without paying
-//   const { tour, user, price } = req.query;
-//   if (!tour && !user && !price) return next();
-
-//   await Booking.create({ tour, user, price });
-
-//   res.redirect(req.originalUrl.split('?')[0])
-// });
- exports.createBookingCheckout = async session => {
+const createBookingCheckout = async session => {
   const tour = session.client_reference_id;
-  const user = await User.findOne({ email: session.customer_email }).id;
-  const price = session.line_items[0].amount / 100;
+  const user = (await User.findOne({ email: session.customer_email })).id;
+  const price = session.display_items[0].amount / 100;
   await Booking.create({ tour, user, price });
- }
+};
 
 exports.webhookCheckout = (req, res, next) => {
-  const signature = req.headers["stripe-signature"];
+  const signature = req.headers['stripe-signature'];
 
   let event;
-
   try {
     event = stripe.webhooks.constructEvent(
       req.body,
@@ -74,11 +62,12 @@ exports.webhookCheckout = (req, res, next) => {
     return res.status(400).send(`Webhook error: ${err.message}`);
   }
 
-  if(event.type === 'checkout.session.complete'){
-    createBookingCheckout(event.data.object)
-  }
-  res.status(200).json({ received: true})
+  if (event.type === 'checkout.session.completed')
+    createBookingCheckout(event.data.object);
+
+  res.status(200).json({ received: true });
 };
+
 exports.createBooking = factory.createOne(Booking);
 exports.getBooking = factory.getOne(Booking);
 exports.getAllBookings = factory.getAll(Booking);
