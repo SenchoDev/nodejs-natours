1eeb89aa5d3b03bfd2dadefea05a0c8160cdf4da
Senadin <beganovicsenadin24@gmail.com>
2020-09-17 15:10:38 +0200

    heroku chagnes

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -6,7 +6,7 @@ export const bookTour = async tourId => {
   try {
     // 1) Get checkout session from API
     const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
-    // console.log(session);
+   console.log(session);
 
     // 2) Create checkout form + chanre credit card
     console.log(stripe)
