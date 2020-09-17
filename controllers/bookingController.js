28cce12166fa1b4db72bf19f67ed85329ab230f9
Senadin <beganovicsenadin24@gmail.com>
2020-09-17 12:40:09 +0200

    heroku push

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -47,6 +47,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   });
 });
 
+
 exports.createBookingCheckout = catchAsync(async (req, res, next) => {
   // This is only TEMPORARY, because it's UNSECURE: everyone can make bookings without paying
   const { tour, user, price } = req.query;
