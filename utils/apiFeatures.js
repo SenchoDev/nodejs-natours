e1b73cf517325b2eddf9567d37bce37550993393
Senadin <beganovicsenadin24@gmail.com>
2020-10-13 20:53:20 +0200

    fully finished tours app

--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -12,13 +12,13 @@ class APIFeatures {
     // 1B) Advanced filtering
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-
     this.query = this.query.find(JSON.parse(queryStr));
     return this;
   }
 
   sort() {
     if (this.queryString.sort) {
+    
       const sortBy = this.queryString.sort.split(',').join(' ');
       this.query = this.query.sort(sortBy);
     } else {
